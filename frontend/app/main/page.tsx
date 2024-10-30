ffc8e95aff8bf2f41eb1e85fedca797d857ba9c0
Chris <totallynotherro@gmail.com>
2024-10-30 17:29:16 -0400

    made old rules page protected

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -1,63 +1,32 @@
-// app/main/pages.tsx
-"use client"; // Mark this file as a Client Component
+"use client";
 
+import { useRouter } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
 import './page.css'; // Import a CSS file for styling
 import Avatar from './avatar';
 import Table from './table';
 import AddPopup from './popup';
+import useCurrentUser from '@/hooks/useCurrentUser';
 
 import axios from "axios"; // Import axios
 axios.defaults.withCredentials = true;
 
 const Pages: React.FC = () => {
+  const router = useRouter();
   const [data, setData] = useState<{ rule: string; grouping: string; description: string }[]>([]);
   const [isPopupOpen, setPopupOpen] = useState(false);
-  const [user, setUser] = useState<{ id: string; email: string; rules: Record<string, Record<string, string>>; refreshToken?: string; createdAt?: string } | null>(null);
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      try {
-        interface User {
-          id: string;
-          Email: string;
-          Rules: Record<string, Record<string, string>>;
-          refreshToken: string;
-          createdAt: string;
-        }
-        interface ResponseData {
-          user?: User;
-        }
-
-        const response: { data: ResponseData } = await axios.get("http://localhost:3010/api/users/current-user");
-
-        if (response.data.user) {
-          const mappedUser = {
-            id: response.data.user.id,
-            email: response.data.user.Email,
-            rules: response.data.user.Rules,
-            refreshToken: response.data.user.refreshToken,
-            createdAt: response.data.user.createdAt,
-          };
-          setUser(mappedUser || null);
-          const formattedData = Object.values(mappedUser.rules).map((rule) => ({
-            rule: rule.action,
-            grouping: rule.prompt,
-            description: rule.type,
-          }));
-          setData(formattedData);
-          console.log("Authenticated user:", mappedUser);
-        } else {
-          setUser(null);
-          console.log("No authenticated user.");
-        }
-      } catch (error) {
-        console.error("Error fetching current user:", error);
-        setUser(null);
-      }
-    };
+  const { user, loading, error } = useCurrentUser();
   
-    fetchCurrentUser();
-  }, []);
+  useEffect(()  => {
+    if (user) {
+      const formattedData = Object.values(user.rules).map((rule) => ({
+        rule: rule.action,
+        grouping: rule.prompt,
+        description: rule.type,
+      }));
+      setData(formattedData);
+    }
+  }, [user])
 
   const handleAddButtonClick = () => {
     if (user) {
@@ -84,7 +53,14 @@ const Pages: React.FC = () => {
     }
   };
 
-  return (
+  if (loading) {
+    return <div>Loading...</div>;
+  } else if (!user) {
+    router.push("/");
+  } else if (error) {
+    return <div>Error: {error}</div>;
+  } else {
+    return (
     <div>
       <Avatar email={user ? user.email : ""} />
       <button onClick={handleAddButtonClick} className="add-button">Add</button>
@@ -96,6 +72,7 @@ const Pages: React.FC = () => {
       />
     </div>
   );
+}
 };
 
 export default Pages;
