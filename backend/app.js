88697f57b0bd19807f19e60a6b356c03ecb09538
Rishi Mantri <rishimantri795@gmail.com>
2025-03-24 17:08:15 -0400

    addeding back s3 declaration cus it disappeared and broke the draft rule. Idk how it disappears cant figure out in the commit history

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,12 @@ app.set("trust proxy", 1); // Trust first proxy
 
 app.use(express.json());
 
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
 const allowedOrigins = [
   "http://localhost:3000",
   "https://theinboxpilot.com",
