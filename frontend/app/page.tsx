e479d8df995490b03e9c78a337e04e04b4fc05cd
Rishi Mantri <rishimantri795@gmail.com>
2025-03-28 00:22:32 -0400

    privacy

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -479,14 +479,14 @@ export default function Component() {
         <p className="text-xs text-gray-500 dark:text-gray-400">
           © 2024 InboxPilot. All rights reserved.
         </p>
-        {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <a className="text-xs hover:underline underline-offset-4" href="#">
+         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          {/*<a className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
-          </a>
-          <a className="text-xs hover:underline underline-offset-4" href="#">
+          </a>*/}
+          <a className="text-xs hover:underline underline-offset-4" href="/privacy">
             Privacy
           </a>
-        </nav> */}
+        </nav> 
       </footer>
     </div>
   );
