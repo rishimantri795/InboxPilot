78540a1fb43c20e0962b024e26f2a8122041c606
Rishi Mantri <rishimantri795@gmail.com>
2025-03-29 11:58:13 -0400

    added google brnding

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -477,9 +477,11 @@ export default function Component() {
       </Dialog>
 
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        {/* <p className="text-xs text-gray-500 dark:text-gray-400">
-          © 2024 InboxPilot. All rights reserved.
-        </p> */}
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          {/* © 2024 InboxPilot. All rights reserved. */}
+          Gmail™ email service is a trademark of Google LLC.<br></br>
+          Google Calendar™ calendaring application is a trademark of Google LLC.
+        </p>
          <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           {/*<a className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
